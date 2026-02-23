2dc5d7b28a24524f3ce1854ef5ff955de07256df
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-02-23 22:56:48 +0800

    feat:添加用于格式化远程仓库路径的辅助函数

--- github/types.go
+++ github/types.go
@@ -1,6 +1,10 @@
 package github
 
-import "time"
+import (
+	"net/url"
+	"strings"
+	"time"
+)
 
 // 用于接受的Issue,不能用于构造发送的请求
 type Issue struct {
@@ -16,4 +20,21 @@ type Issue struct {
 type User struct {
 	Login   string
 	HTMLURL string `json:"html_url"`
+}
+
+func FormatRepoPath(target string) (string, error) {
+	u, err := url.Parse(target)
+	//尝试解析https风格url
+	if err != nil && u.Scheme != "" {
+		path := u.Path
+		path = strings.Trim(path, "/") //去除首尾'/'
+		path = strings.TrimSuffix(path, ".git")
+		return path, err
+	}
+	//尝试解析scp风格url
+	idx := strings.Index(target, ":") //返回第一个:的位置
+	path := target[idx+1:]
+	path = strings.Trim(path, "/")
+	path = strings.TrimSuffix(path, ".git")
+	return path, err
 }
